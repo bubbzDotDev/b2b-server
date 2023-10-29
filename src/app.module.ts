1d7cf6c6b5bbf8010eed34f9283aea96978285e1
Christopher Blount <bubbz@bubbz.dev>
2023-10-29 06:18:11 -0600

    Updates dependencies. Removes AI21.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { OpenAIModule } from './open-ai/open-ai.module';
-import { OpenAIModule as OpenAIConfigModule } from '@platohq/nestjs-openai/dist/openai.module';
 import { AI21Module } from './ai21/ai21.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
-    }),
-    OpenAIConfigModule.register({
-      apiKey: process.env.OPENAI_API_KEY,
     }),
     OpenAIModule,
     AI21Module,
--- src/open-ai/open-ai.controller.ts
+++ src/open-ai/open-ai.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { OpenAIService } from './open-ai.service';
-import {
-  ChatCompletionRequestMessage,
-  CreateChatCompletionResponse,
-  CreateModerationRequestInput,
-  CreateModerationResponse,
-} from 'openai';
+import { MessageObjectOpenAI } from './types/message';
+import { OpenAI } from 'openai';
+import ChatCompletion = OpenAI.ChatCompletion;
+import { ModerationCreateResponse } from 'openai/resources';
 
 @Controller('api/open-ai')
 export class OpenAIController {
@@ -13,15 +11,15 @@ export class OpenAIController {
 
   @Post('generate')
   createCompletion(
-    @Body('input') input: ChatCompletionRequestMessage[],
-  ): Promise<CreateChatCompletionResponse> {
+    @Body('input') input: MessageObjectOpenAI[],
+  ): Promise<ChatCompletion> {
     return this.openAIService.createChatCompletion(input);
   }
 
   @Post('moderate')
   createModeration(
-    @Body('input') input: CreateModerationRequestInput,
-  ): Promise<CreateModerationResponse> {
+    @Body('input') input: string,
+  ): Promise<ModerationCreateResponse> {
     return this.openAIService.createModeration(input);
   }
 }
